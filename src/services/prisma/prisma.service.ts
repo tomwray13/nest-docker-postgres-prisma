fdae7fd2ab2c408564b3e1ff769574e969f0458b
Tom <tomwray13@gmail.com>
2022-06-06 22:39:36 +0100

    Make Prisma client abstraction

--- src/services/prisma/prisma.service.ts
+++ src/services/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}

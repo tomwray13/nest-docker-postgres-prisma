9cc98b6d30c0034c2a979b5433f4c824ee0107e4
Tom <tomwray13@gmail.com>
2022-06-06 22:36:23 +0100

    Run nest g module services/prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './services/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })

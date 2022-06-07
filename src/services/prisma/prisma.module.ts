c706f7443fed53983c32f8e0be62db45202a0508
Tom <tomwray13@gmail.com>
2022-06-07 22:39:04 +0100

    Ensure prisma service is in module exports

--- src/services/prisma/prisma.module.ts
+++ src/services/prisma/prisma.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  providers: [PrismaService]
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
